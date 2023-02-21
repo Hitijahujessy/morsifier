6c24954a15fd7f645584b93d6714a78876b6938d
Hitijahujessy <91663510+Hitijahujessy@users.noreply.github.com>
2023-02-21 12:52:00 +0100

    translate_to_morse() now only translates to morsecode, new type_morse() function adds the typewriter effect

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from kivy.lang import Builder
 from kivy.properties import BooleanProperty, ObjectProperty
 from kivy.uix.widget import Widget
 
-os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2' # Enable to prevent OpenGL error
+# os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2' # Enable to prevent OpenGL error
 root_widget = Builder.load_file('app.kv')
 
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '1':'.----', '2':'..---', '3':'...--',
-                    '4':'....-', '5':'.....', '6':'-....',
-                    '7':'--...', '8':'---..', '9':'----.',
-                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
-                    '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-'}
+MORSE_CODE_DICT = { 'A':' .- ', 'B':' -... ',
+                    'C':' -.-. ', 'D':' -.. ', 'E':' . ',
+                    'F':' ..-. ', 'G':' --. ', 'H':' .... ',
+                    'I':' .. ', 'J':' .--- ', 'K':' -.- ',
+                    'L':' .-.. ', 'M':' -- ', 'N':' -. ',
+                    'O':' --- ', 'P':' .--. ', 'Q':' --.- ',
+                    'R':' .-. ', 'S':' ... ', 'T':' - ',
+                    'U':' ..- ', 'V':' ...- ', 'W':' .-- ',
+                    'X':' -..- ', 'Y':' -.-- ', 'Z':' --.. ',
+                    '1':' .---- ', '2':' ..--- ', '3':' ...-- ',
+                    '4':' ....- ', '5':' ..... ', '6':' -.... ',
+                    '7':' --... ', '8':' ---.. ', '9':' ----. ',
+                    '0':' ----- ', ', ':' --..-- ', '.':' .-.-.- ',
+                    '?':' ..--.. ', '/':' -..-. ', '-':' -....- ',
+                    '(':' -.--. ', ')':' -.--.- '}
 
 # threading multiprocessing
 
@@ -34,10 +34,24 @@ class MainWidget(Widget):
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
-        self.typewriter = Clock.create_trigger(self.translate_to_morse, .25)
+        self.typewriter = Clock.create_trigger(self.type_morse, .25)
         self.morse_loop = Clock.create_trigger(self.repeat, .25)
 
-    def translate_to_morse(self, dt):
+
+    def translate_to_morse(self):
+        
+        self.string = self.string.replace(" ", " + ")
+
+        for char in self.string:
+            if char in MORSE_CODE_DICT:
+                #char = MORSE_CODE_DICT[char]
+                self.string = self.string.replace(char, MORSE_CODE_DICT[char])
+
+        self.string = self.string.replace("+", "/")
+        self.string += " "
+
+    def type_morse(self, dt):
+
         self.ids.morse_label.text += self.string[0]
         self.string = self.string[1:]
         if len(self.string) > 0:
